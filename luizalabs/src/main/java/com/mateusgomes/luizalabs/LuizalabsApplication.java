6054f61c09422048019fce7a3ee04e634b98ea96
Mateus Gomes <magomescruz@gmail.com>
2023-03-16 20:38:25 -0300

    :tada: Setup project

---
+++ luizalabs/src/main/java/com/mateusgomes/luizalabs/LuizalabsApplication.java
@@ -0,0 +1,13 @@
+package com.mateusgomes.luizalabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuizalabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuizalabsApplication.class, args);
+	}
+
+}
