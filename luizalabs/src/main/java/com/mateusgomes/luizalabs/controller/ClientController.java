6631230799778a493e386dc8a3a119146e064baf
Mateus Gomes <magomescruz@gmail.com>
2023-03-17 20:29:48 -0300

    :sparkles: Add endpoint GET /clients/{idClient}

--- luizalabs/src/main/java/com/mateusgomes/luizalabs/controller/ClientController.java
+++ luizalabs/src/main/java/com/mateusgomes/luizalabs/controller/ClientController.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/clients")
@@ -23,10 +26,20 @@ public class ClientController {
         List<Client> clientsList = clientService.findAll();
 
         if (clientsList.isEmpty()){
-            return new ResponseEntity<>(clientsList, HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } else {
             return new ResponseEntity<>(clientsList, HttpStatus.OK);
         }
+    }
 
+    @GetMapping("/{idClient}")
+    public ResponseEntity findClientById(@PathVariable UUID idClient){
+        Optional<Client> optionalClient = clientService.findById(idClient);
+
+        if(optionalClient.isPresent()){
+            return new ResponseEntity<>(optionalClient, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
     }
 }
--- luizalabs/src/main/java/com/mateusgomes/luizalabs/service/ClientService.java
+++ luizalabs/src/main/java/com/mateusgomes/luizalabs/service/ClientService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class ClientService {
@@ -15,5 +17,9 @@ public class ClientService {
 
     public List<Client> findAll() {
         return clientRepository.findAll();
+    }
+
+    public Optional<Client> findById(UUID clientId) {
+        return clientRepository.findById(clientId);
     }
 }
